399eb7f2ae7f8a217f2d032ea6f547c0bb3ad523
Jeremy Louet <jeremy.louet29@hotmail.fr>
2024-05-14 15:42:10 +0200

    Modifications mineures Header

--- src/components/Carroussel.jsx
+++ src/components/Carroussel.jsx
@@ -1,13 +1,49 @@
-import React from 'react'
-// import Data from '../data/Data.json'
+import React, { useState } from 'react'
+import ArrowRight from '../assets/arrow-right.png'
+import ArrowLeft from '../assets/arrow-left.png'
+import '../styles/Carroussel.scss'
 
 
-function Carroussel() {
+ function Carroussel({pictures}) {
+  const [ currentPictures, setCurrentPictures] = useState(0)
+
+  function changePictures(direction) {
+    if(direction === "next") {
+      setCurrentPictures((prevPicture) => (prevPicture +1) % pictures.length)
+    } else if (direction === "previous") {
+      setCurrentPictures((prevPicture) => prevPicture === 0 ? pictures.length -1 : prevPicture - 1)
+    }
+  }
+
   return (
-    <div>
-        Carroussel
+    <div className='carroussel'>
+        {pictures.length > 1 && (
+          <img src={ArrowLeft} 
+              className='carroussel__arrow-left'
+              alt="arrow-left"
+              onClick={() => changePictures("previous")}
+          />
+        )}
+
+        <img src={pictures[currentPictures]}
+              alt="pictures-next"
+              className='carroussel__picture'
+         />
+
+         {pictures.length > 1 && (
+          <img src={ArrowRight}
+              className='carroussel__arrow-right'
+              alt='arrow-right'
+              onClick={() => changePictures("next")}
+          />
+         )}
+
+         {pictures.length > 1 && (
+          <p className='carroussel__number'>{currentPictures +1}/{pictures.length}</p>
+         )}
+
     </div>
   )
 }
 
-export default Carroussel
+export default Carroussel 
--- src/pages/Flats/Flats.jsx
+++ src/pages/Flats/Flats.jsx
@@ -3,12 +3,19 @@ import '../../styles/Flats.scss'
 // import Carroussel from '../../components/Carroussel'
 // import Collapse from '../../components/Collapse'
 // import ImgTest from '../../assets/Background-test.png'
-// import Data from '../../data/Data.json'
+import Data from '../../data/Data.json'
+import Carroussel from '../../components/Carroussel'
 
 function Flats() {
   return (
     <div className='appart'>
-      Flats
+      <Carroussel pictures={Data.pictures} />
+
+      <h1>{Data.title}</h1>
+      <h3>{Data.location}</h3>
+      <div>
+
+      </div>
     </div>
   )
 }
