86971f4b38bebec983443b176a44cc4acc19db18
Jeremy Louet <jeremy.louet29@hotmail.fr>
2024-05-22 15:08:40 +0200

    Modifications de style  mineures

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import Banner from '../../components/Banner'
 import ImgBannerHome from '../../assets/img-banner.png'
 import FlatGrid from '../../components/FlatGrid'
+import '../../styles/Home.scss'
 
 function Home() {
   return (
-    <div>
+    <div className='home'>
       <Banner image={ImgBannerHome} title="Chez vous, partout et ailleurs" />
       <FlatGrid />
     </div>
