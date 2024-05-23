f794f11dec0c74c414b29625c3b880ab79b1a8d3
Jeremy Louet <jeremy.louet29@hotmail.fr>
2024-05-23 12:00:24 +0200

    Version finale

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import Banner from '../../components/Banner'
 import ImgBannerHome from '../../assets/img-banner.png'
 import FlatGrid from '../../components/FlatGrid'
-import '../../styles/Home.scss'
 
 function Home() {
   return (
