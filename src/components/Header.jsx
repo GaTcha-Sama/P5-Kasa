026bd08f5d2295f18b1d811372e1b00ffb29962d
Jeremy Louet <jeremy.louet29@hotmail.fr>
2024-05-24 09:12:42 +0200

    Modif mineure version finale

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,24 @@
 import React from 'react';
-import '../styles/Header.scss'
-import Logo from '../assets/logo.png'
-import { Link } from 'react-router-dom'
+import '../styles/Header.scss';
+import Logo from '../assets/logo.png';
+import { Link, useLocation } from 'react-router-dom';
 
 function Header() {
+    const location = useLocation();
+    
     return ( 
         <nav className="navbar">
             <div className="navbar__logo">
                 <img src={Logo} alt="logo" />
             </div>
-            <Link to="/" style={{ textDecoration: 'none'}}><div className="navbar__txt">Accueil</div></Link>
-            <Link to="/about" style={{ textDecoration: 'none'}}><div className="navbar__txt">À Propos</div></Link>
+            <Link to="/" style={{ textDecoration: 'none' }}>
+                <div className={`navbar__txt ${location.pathname === '/' ? 'active' : ''}`}>Accueil</div>
+            </Link>
+            <Link to="/about" style={{ textDecoration: 'none' }}>
+                <div className={`navbar__txt ${location.pathname === '/about' ? 'active' : ''}`}>À Propos</div>
+            </Link>
         </nav>
-    )
+    );
 }
 
-export default Header
+export default Header;
